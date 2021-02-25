c01bdbb6d25dfabb52fcb0ca0cf8c4235a4777bd
jaydencellucci <jayden.cellucci@ocsbstudent.ca>
2021-02-25 19:28:13 +0000

    lining, punctuation, spacing and indenting

--- script.js
+++ script.js
@@ -2,21 +2,20 @@
 function formula () {
   let a = document.getElementById('t1').value
   a = parseInt(a)
-  area(a);
+  area(a)
 }
-
 
 //  calcucate the area using the fucntion (formula)
 function area (a) {
   const answer = (3.14 * a * a)
-  console.log(answer);  
+  console.log(answer)
 }
 
 //  Enter in the circumference in the second text box
 function formula2 () {
-  let c= document.getElementById('t2').value;
+  let c = document.getElementById('t2').value;
   c = parseInt(c)
-  circumference (c);
+  circumference (c)
 }
 
 //  calculate the circumference using the function (formula2)

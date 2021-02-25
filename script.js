0b53d69c45dc0a1a7356cdb794e754e2451369d6
jaydencellucci <jayden.cellucci@ocsbstudent.ca>
2021-02-25 19:09:19 +0000

    -java script spacing and indenting

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-//Enter in the a area in the first textboxt
-function formula(){
-  let a = document.getElementById("t1").value;
+//  Enter in the a area in the first textbox
+function formula () {
+  let a = document.getElementById('t1').value
   a = parseInt(a)
-  area (a);
+  area(a);
 }
 
 
-//calcucate the area using the fucntion (formula)
-function area(a) {
-  const answer = (3.14*a*a)
-  console.log(answer);
-    
+//  calcucate the area using the fucntion (formula)
+function area (a) {
+  const answer = (3.14 * a * a)
+  console.log(answer);  
 }
-function formula2(){
-  let c= document.getElementById("t2").value;
 
+//  Enter in the circumference in the second text box
+function formula2 () {
+  let c= document.getElementById('t2').value;
   c = parseInt(c)
   circumference (c);
 }
 
-function circumference(c) {
-  const answer = (2*3.14*c)
+//  calculate the circumference using the function (formula2)
+function circumference (c) {
+  const answer = (2 * 3.14 * c)
   console.log(answer)
 }

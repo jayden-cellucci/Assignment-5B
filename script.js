0dc23952d2d8107a45d4a1c69e5a19e2b8ad3300
jaydencellucci <jayden.cellucci@ocsbstudent.ca>
2021-02-25 17:14:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+//Enter in the a area in the first textboxt
+function formula(){
+  let a =document.getElementById("t1").value;
+  a = parseInt(a)
+  area (a);
+}
+
+
+//calcucate the area using the fucntion (formula)
+function area(a) {
+  const answer = (3.14*a*a)
+  console.log(answer);
+    
+}
+function formula2(){
+  let c= document.getElementById("t2").value;
+
+  c = parseInt(c)
+  circumference (c);
+}
+
+function circumference(c) {
+  const answer = (2*3.14*c)
+  console.log(answer)
+}
